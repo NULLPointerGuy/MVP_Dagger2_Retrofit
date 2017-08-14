464779ca07d3ba0a09947c32e5a3e337ed9ea1f0
callmekarthik <karthik.rk18@gmail.com>
2017-08-14 23:17:42 +0530

    Few more cases added

--- app/src/test/java/com/karthik/todo/TodoTestPresenter.java
+++ app/src/test/java/com/karthik/todo/TodoTestPresenter.java
@@ -5,15 +5,27 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.karthik.todo.APIService.ForecastAPIManager;
 import com.karthik.todo.APIService.UnsplashAPIManager;
 import com.karthik.todo.DB.Dbhander;
+import com.karthik.todo.Pojo.Result;
+import com.karthik.todo.Pojo.Unsplash;
 import com.karthik.todo.Screens.Todo.MVP.TodoPresenter;
 import com.karthik.todo.Screens.Todo.MVP.TodoPresenterContract;
 import com.karthik.todo.Screens.Todo.MVP.TodoViewContract;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -48,5 +60,46 @@ public class TodoTestPresenter {
         when(dbhander.isDbEmpty()).thenReturn(true);
         mockPresenter.loadTasks();
         verify(mockView,times(1)).showEmptyTextAndHideTask();
+    }
+
+    @Test
+    public void whenTaskIsNotEmptyHideEmptyTextAndShowTaskList(){
+        when(dbhander.isDbEmpty()).thenReturn(false);
+        mockPresenter.loadTasks();
+        verify(mockView,times(1)).hideEmptyTextAndShowTask();
+        verify(mockView,times(1)).loadTasks(dbhander.getAllTodo());
+    }
+
+    @Test
+    public void whenAddTodoClickedOpenTodoScreen(){
+        mockPresenter.onAddTodoClicked();
+        verify(mockView,times(1)).openAddTodoScreen();
+    }
+
+    @Test
+    public void verifySetDashTitle(){
+        Calendar cal = Calendar.getInstance();
+        String formatedDateString = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault())
+                + ", " + cal.get(Calendar.DAY_OF_MONTH)
+                + " " + cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()) + " ";
+        mockPresenter.setDashTitle();
+        Assert.assertNotEquals(null,formatedDateString);
+        Assert.assertNotEquals("",formatedDateString);
+        verify(mockView,times(1)).setDashBoardTitle(formatedDateString);
+    }
+
+    @Test
+    public void testStringValueOfToday(){
+        Assert.assertNotEquals(null,String.valueOf(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)));
+        Assert.assertNotEquals("",String.valueOf(Calendar.getInstance().get(Calendar.DAY_OF_MONTH)));
+    }
+
+    @Test
+    public void loadUnsplashFromNetworkIfCacheNotPresent(){
+        when(mockView.isCachePresent(String
+                .valueOf(Calendar.getInstance().get(Calendar.DAY_OF_MONTH))))
+                .thenReturn(false);
+        mockPresenter.getUnsplashImages("random");
+        verify(unsplashAPIManager,times(1)).getPhotosList("random");
     }
 }
